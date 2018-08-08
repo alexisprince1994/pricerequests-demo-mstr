2c9d7219cd58d42c8e15b258e9d8b7f434d1985d
Alex Cano <alexjcano1994@gmail.com>
2018-08-07 18:20:37 -0700

    created user auth

---
+++ src/server/user/forms.py
@@ -0,0 +1,35 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from server.models import User
+
+
+class RegistrationForm(FlaskForm):
+
+	username = StringField('Username', 
+		validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password',
+		validators=[DataRequired(), Length(min=6, max=50)])
+	confirm_password = PasswordField('Confirm Password',
+		validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Register')
+
+	def validate_username(self, username):
+		user = User.query.filter_by(username=username.data).first()
+		if user is not None:
+			raise ValidationError('This username is taken. Please select a different one.')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is not None:
+			raise ValidationError('There is already a user registered with this email.')
+
+class LoginForm(FlaskForm):
+
+	email = StringField('Email', 
+		validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	remember = BooleanField('Remember Me')
+
+	submit = SubmitField('Login')
