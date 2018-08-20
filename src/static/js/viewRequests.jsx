ccafa21f12f20229039a55779b11234142e02c97
Alex Cano <alexjcano1994@gmail.com>
2018-08-20 10:15:51 -0700

    removing references to old folder structure

--- src/static/js/viewRequests.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PriceRequestManager from './approval/pricerequestmanager'
-
-ReactDOM.render(<PriceRequestManager />, document.getElementById('content'))
