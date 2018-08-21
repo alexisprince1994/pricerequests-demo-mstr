a24c2c281c0cb7654f3ba3909a5bf985d6812ddc
Alex Cano <alexjcano1994@gmail.com>
2018-08-20 18:52:29 -0700

    added command line support for running tests

---
+++ manage.py
@@ -0,0 +1,19 @@
+from flask_script import Manager
+from app import app, db, models
+import unittest
+
+manager = Manager(app)
+
+@manager.command
+def test():
+	"""
+	Runs tests without coversage
+	"""
+	tests = unittest.TestLoader().discover('tests', pattern='test*.py')
+	result = unittest.TextTestRunner(verbosity=2).run(tests)
+	if result.wasSuccessful():
+		return 0
+	return 1
+
+if __name__ == "__main__":
+	manager.run()
