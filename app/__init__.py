b82cfb17d8a48cfd506b690bbab04194797c9b8f
Alex Cano <alexjcano1994@gmail.com>
2018-09-14 17:43:43 -0700

    added proper factory function

--- app/__init__.py
+++ app/__init__.py
@@ -12,45 +12,61 @@ from flask_wtf.csrf import CSRFProtect
 # Internal Packages are listed below in order to avoid
 # circular imports.
 
-app = Flask(__name__, template_folder='./static', static_folder='./static/dist')
+# Intializating extensions allowing them to be imported from
+# other files
 
-# Loading config path from environment variables or defaulting to dev
-app_settings = os.environ.get(
-	'APP_SETTINGS',
-	'app.config.DevelopmentConfig'
-)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+csrf = CSRFProtect()
+login_manager = LoginManager()
 
-app.config.from_object(app_settings)
-# Initialize extensions
-
-# Flask Sqlalchemy for database
-db = SQLAlchemy(app)
-# Flask Bcrypt for password hashing
-bcrypt = Bcrypt(app)
-
-# Cross Site Request Forgery protection
-csrf = CSRFProtect(app)
-
-# Flask Login for login management
-login_manager = LoginManager(app)
-
+# Import required after db declaration
 from .models import User
 
 @login_manager.user_loader
 def user_loader(id):
 	return User.query.get(id)
 
-# Blueprint imports
-from .user.views import users
-from .pricerequests.views import pricerequest
-from .homepage.views import homepage
-from .api.views import api
-from .editrefs.views import editrefs
+def create_app(config=None):
 
-# Registering blueprints
-app.register_blueprint(users)
-app.register_blueprint(pricerequest)
-app.register_blueprint(homepage)
-app.register_blueprint(api)
-app.register_blueprint(editrefs)
-login_manager.login_view = 'users.login'
+	app = Flask(__name__, template_folder='./static', static_folder='./static/dist')
+
+	# Loading config path from environment variables or defaulting to dev
+	if config is None:
+		config = os.environ.get(
+			'APP_SETTINGS',
+			'app.config.DevelopmentConfig'
+		)
+
+	app.config.from_object(config)
+	# Initialize extensions with app object
+
+	# Flask Sqlalchemy for database
+	db.init_app(app)
+	# Flask Bcrypt for password hashing
+	bcrypt.init_app(app)
+
+	# Cross Site Request Forgery protection
+	csrf.init_app(app)
+
+	# Flask Login for login management
+	login_manager.init_app(app)
+
+	# Blueprint imports
+	from .user.views import users
+	from .pricerequests.views import pricerequest
+	from .homepage.views import homepage
+	from .api.views import api
+	from .editrefs.views import editrefs
+
+	# Registering blueprints
+	app.register_blueprint(users)
+	app.register_blueprint(pricerequest)
+	app.register_blueprint(homepage)
+	app.register_blueprint(api)
+	app.register_blueprint(editrefs)
+	login_manager.login_view = 'users.login'
+
+	return app
+
+app = create_app()
