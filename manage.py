f217951dcc19fc1c133dc83e74dbbbb38a20252c
Alex Cano <alexjcano1994@gmail.com>
2018-11-25 15:23:21 -0800

    added utility to clean database and rebuild

--- manage.py
+++ manage.py
@@ -1,6 +1,9 @@
 from flask_script import Manager
-from app import app, db, models
+from app import app, db, models, bcrypt
+from app.data import create_data
+from app.models import Product, Customer, PriceRequestStatus, User
 import unittest
+import os
 
 manager = Manager(app)
 
@@ -14,6 +17,32 @@ def test():
 	if result.wasSuccessful():
 		return 0
 	return 1
+
+@manager.command
+def reset_db():
+	"""
+	Drops and recreates the database. Used only in development.
+	Will throw an error if app is in production mode.
+	"""
+
+	if not app.config.get('DEBUG'):
+		raise ValueError('Cannot rewrite database outside of development')
+
+	db.drop_all()
+	db.create_all()
+
+	create_data(Product=Product, Customer=Customer, db=db, 
+		PriceRequestStatus=PriceRequestStatus)
+
+	admin_password = os.environ.get('ADMIN_PASSWORD')
+	hashed_admin_password = bcrypt.generate_password_hash(admin_password).decode('utf-8')
+	admin = User(username='admin', email='admin@example.com', password=hashed_admin_password,
+		active=True, read_only=False)
+	db.session.add(admin)
+	db.session.commit()
+
+
+
 
 if __name__ == "__main__":
 	manager.run()
